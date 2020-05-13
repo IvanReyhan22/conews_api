0c5e3f6a2dea839b6fe7e41e657949407c400756
IvanReyhan22 <43955677+IvanReyhan22@users.goes.github.com>
2020-05-13 13:42:23 +0700

    fix conn database

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 4000,
+    bodyParser = require('body-parser'),
+    controller = require('./controller');
+    serveIndex = require('serve-index');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/storage', express.static('storage'), serveIndex('storage'))
+
+var routes = require('./routes');
+routes(app);
+
+app.listen(port);
+console.log('Server started on port: ' + port);
