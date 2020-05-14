958a8245503a5cab8512aedc3a7fbafae388cc1c
IvanReyhan22 <43955677+IvanReyhan22@users.goes.github.com>
2020-05-14 10:06:28 +0700

    (+) Set timeout database connection delay err

--- conn.js
+++ conn.js
@@ -2,17 +2,58 @@ var mysql = require('mysql');
 
 var database = "w2NUkoaNPK"
 
-var con = mysql.createConnection({
+var db_config = {
     host: "remotemysql.com",
     user: "w2NUkoaNPK",
     password: "20mBA2bR4T",
     database: database
-});
+}
 
+// var con = mysql.createConnection({
+//     host: "remotemysql.com",
+//     user: "w2NUkoaNPK",
+//     password: "20mBA2bR4T",
+//     database: database
+// });
 
-con.connect(function (err){
-    if(err) throw console.log(err);
-    else console.log("Database Status : Connected\nDatabase Name : " + database)
-});
+var connection;
+
+function handleDisconnect() {
+
+    connection = mysql.createConnection(db_config);
+
+    connection.connect(function (err) {
+
+        if (err) {
+            console.log('error when connecting to database: ', err)
+            setTimeout(handleDisconnect, 2000)
+        }
+
+    })
+
+    connection.on('error', function (err) {
+
+        console.log('db error ', err);
+
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+
+            handleDisconnect();
+
+        } else {
+
+            throw err;
+
+        }
+
+    });
+
+}
+
+handleDisconnect()
+
+// con.connect(function (err) {
+//     if (err) throw console.log(err);
+//     else console.log("Database Status : Connected\nDatabase Name : " + database)
+// });
 
 module.exports = con;
