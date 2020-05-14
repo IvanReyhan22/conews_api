095b82923ec9a668a03a4212312a871c121772e1
IvanReyhan22 <43955677+IvanReyhan22@users.goes.github.com>
2020-05-14 12:08:54 +0700

    Throw function cause aborting the node program response

--- controller.js
+++ controller.js
@@ -83,7 +83,7 @@ exports.loginUser = function (req, res) {
     conn.query('SELECT * FROM tb_user WHERE email = ?',
         [eml]
         , function (error, rows, fields) {
-            if (error) throw response.err("Request Timed Out / No Internet Access", res)
+            if (error) return response.err("Request Timed Out / No Internet Access " +error, res)
 
             if (rows.length != 0) {
 
